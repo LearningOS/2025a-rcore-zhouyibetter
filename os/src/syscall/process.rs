ada650c124063a6793142af3f1387cdb2db4e2b0
zhouyibetter <zhouyibetter@163.com>
2025-10-12 02:29:15 +0800

    初步完成sys_get_time和sys_trace

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,5 +1,14 @@
 //! Process management syscalls
-use crate::task::{change_program_brk, exit_current_and_run_next, suspend_current_and_run_next};
+use core::mem::size_of;
+
+use crate::{
+    mm::{translated_byte_buffer, PageTable},
+    task::{
+        change_program_brk, current_user_token, exit_current_and_run_next,
+        suspend_current_and_run_next,
+    },
+    timer::get_time_us,
+};
 
 #[repr(C)]
 #[derive(Debug)]
@@ -25,16 +34,59 @@ pub fn sys_yield() -> isize {
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     trace!("kernel: sys_get_time");
-    -1
+    let us = get_time_us();
+    let time_val = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+    let buffers =
+        translated_byte_buffer(current_user_token(), ts as *const u8, size_of::<TimeVal>());
+
+    let bytes = unsafe {
+        core::slice::from_raw_parts(
+            &time_val as *const TimeVal as *const u8,
+            size_of::<TimeVal>(),
+        )
+    };
+    let mut current = 0;
+    for buffer in buffers {
+        let len = buffer.len().min(bytes.len() - current);
+        buffer[..len].copy_from_slice(&bytes[current..current + len]);
+        current += len;
+    }
+    0
 }
 
 /// TODO: Finish sys_trace to pass testcases
 /// HINT: You might reimplement it with virtual memory management.
-pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
+pub fn sys_trace(trace_request: usize, id: usize, data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    match trace_request {
+        0 => {
+            let ptr = id as *const u8;
+            let buffers = translated_byte_buffer(current_user_token(), ptr, 1);
+            if buffers.is_empty() || buffers[0].is_empty() {
+                return -1;
+            }
+            let byte_value = buffers[0][0];
+            byte_value as isize
+        },
+        1 => {
+            let ptr = id as *mut u8;
+            let mut buffers = translated_byte_buffer(current_user_token(), ptr, 1);
+            if buffers.is_empty() || buffers[0].is_empty() {
+                return -1;
+            }
+            buffers[0][0] = data as u8;
+            0
+        },
+        2 => {
+            0
+        },
+        _ => -1,
+    }
 }
 
 // YOUR JOB: Implement mmap.
